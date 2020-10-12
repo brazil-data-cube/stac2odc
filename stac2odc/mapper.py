1628574928c4a5fdd0f99ae14b891addaa79e9e9
Felipe Carlos <efelipecarlos@gmail.com>
2020-10-12 08:39:19 -0300

    Introducing StacMapperEngine

--- stac2odc/mapper.py
+++ stac2odc/mapper.py
@@ -6,316 +6,134 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
-from abc import ABC, abstractmethod
-
-from loguru import logger
-import stac2odc.utils as utils
 from collections import OrderedDict
-from stac.item import Item
-from stac.collection import Collection
+from stac2odc.ioa import load_custom_configuration_file
+from stac2odc.exception import EngineDefinitionNotFound
 
 
-class StacMapper(ABC):
-    @abstractmethod
-    def collection_to_product(self, stac_collection: Collection, custom_fields: dict) -> OrderedDict:
-        pass
+class StacMapperEngine:
+    DEFAULT_ENGINE_DEFINITION = {
+        'STAC09': 'engine-definitions/stac_mapper_v09.json'
+    }
 
-    @abstractmethod
-    def item_to_dataset(self, stac_item: Item, custom_fields: dict) -> OrderedDict:
-        pass
+    def __init__(self, engine_definition_file=None, engine_type=None):
+        """StacMapperEngine is a core of stac2odc tool. An engine is able to map the STAC definition to ODC definition
+        Args:
+            engine_definition_file (str): File with StacMapperEngine definition
+            engine_type (str): Name of pre-registered StacMapperEngine definition
+        """
 
+        if not engine_definition_file:
+            if engine_type:
+                engine_definition_file = self.DEFAULT_ENGINE_DEFINITION[engine_type]
+            else:
+                raise EngineDefinitionNotFound("Any engine definition was not found")
 
-class Stac09Mapper(StacMapper):
-    def collection_to_product(self, stac_collection: Collection, custom_fields: dict) -> OrderedDict:
-        pass
+        self._engine_definition = load_custom_configuration_file(engine_definition_file)
 
-    def item_to_dataset(self, stac_item: Item, custom_fields: dict) -> OrderedDict:
-        pass
+    def map_collection_to_product(self, stac_collection: dict):
+        """Method to map STAC Collections to Products using engine's selected rules
+        Args:
+            stac_collection (dict): STAC Collection properties
+        Returns:
+            OrderedDict: ODC Product created using STAC Collection definitions
+        """
 
-    def _build_custom_tree(self, odc_element: OrderedDict):
-        pass
+        def apply_custom_mapping_in_stac_values(stac_values: list, custom_mapping: dict):
+            """Function to map custom definitions in mapping function. With this functions is possible use STAC
+            definition in arbitrary fields
+            Args:
+                stac_values (list): List of STAC Items
+                custom_mapping (dict): Dict with custom mapping, where key is used in ODC's definition and values is
+                recovered from STAC elements
 
-"""
-class Stac2ODCMapper(ABC):
-    @abstractmethod
-    def map_collection(self, collection, **kwargs) -> OrderedDict:
-        pass
+            Returns:
+                list: List with STAC Items mapped
+            """
+            stac_values_with_custom_fields = []
+            for _stac_value in stac_values:
+                _stac_value_to_map = OrderedDict()
 
-    @abstractmethod
-    def map_dataset(self, collection, dataset_items, **kwargs) -> OrderedDict:
-        pass
+                for custom_key_mapping in custom_mapping:
+                    _stac_value_to_map[custom_key_mapping] = _stac_value.get(
+                        custom_mapping.get(custom_key_mapping)
+                    )
+                stac_values_with_custom_fields.append(_stac_value_to_map)
+            return stac_values_with_custom_fields
 
+        odc_product_definition = OrderedDict()
+        collection_mapper = self._engine_definition.get('collection')
+        product_definition = collection_mapper.get('fromSTAC')
 
-class Stac2ODCMapper08(Stac2ODCMapper):
+        for product_property in product_definition:
+            property_definition = product_definition.get(product_property)
 
-    def generate_product_type(self, collection):
-        if collection['bdc:temporal_composition']:
-            return "{}_{}_{}".format(
-                collection['properties']['bdc:temporal_composition']['schema'],
-                collection['properties']['bdc:temporal_composition']['step'],
-                collection['properties']['bdc:temporal_composition']['unit'])
-        return None
+            if 'customMapping' in property_definition:
+                stac_value = self._get_value_by_tree_path(stac_collection, property_definition.get('from'))
+                stac_value = apply_custom_mapping_in_stac_values(stac_value, property_definition.get('customMapping'))
+            else:
+                stac_value = self._get_value_by_tree_path(stac_collection, product_definition.get(product_property))
+            self._add_value_by_tree_path(odc_product_definition, product_property, stac_value)
+        return odc_product_definition
 
-    def map_collection(self, collection, **kwargs) -> OrderedDict:
-        def measurements(tag, data):
-            m = OrderedDict()
-            m['name'] = tag
-            m['aliases'] = [data['name'], ]
-            m['dtype'] = data['data_type'].lower()
-            m['nodata'] = data['fill']
-            m['units'] = kwargs['units']
-            return m
+    def _get_value_by_tree_path(self, element: dict, tree_path: str):
+        """This method gets value from a dict using a string path separated with points.
+        e. g.
+            using the following dict:
+                person = {'person': {'surname': 'blabla', 'age': 25}}
+            to get the surname:
+                _get_value_by_tree_path(person, 'person.surname')
 
-        if kwargs['verbose']:
-            logger.info("collection2product is running!")
+        Args:
+            element (dict): Element to get values using tree path
+            tree_path (str): String separated with points representing the tree path
+        Returns:
+            recovered value using tree_path
+        """
+        element = element.copy()
+        tree_path = tree_path.split('.')
 
-        crs_proj4 = collection['properties']['bdc:crs']
-        if kwargs['is_pre_collection']:
-            crs_proj4 = utils.fix_precollection_crs(crs_proj4)
+        for tree_node in tree_path:
+            element = element[tree_node]
+        return element
 
-        odc_config = OrderedDict()
-        odc_config['name'] = collection['id']
-        odc_config['description'] = collection['description']
-        odc_config['metadata_type'] = kwargs['metadata_type']
+    def _add_value_by_tree_path(self, element: dict, tree_path: str, value: object) -> None:
+        """Add values in dictionary using path separated with points
+        Args:
+            element (dict): Element where value is inserted (in-place)
+            tree_path (str): String separated with points representing the tree path
+            value (object): Value to be inserted
+        Returns:
+            None
+        """
+        tree_path = tree_path.split('.')
 
-        odc_config['storage'] = OrderedDict()
-        odc_config['storage']['crs'] = crs_proj4
-        odc_config['storage']['resolution'] = OrderedDict()
-        first_band = next(iter(collection['properties']['bdc:bands']))
-        odc_config['storage']['resolution']['x'] = int(
-            collection['properties']['bdc:bands'][first_band]['resolution_x'])
-        odc_config['storage']['resolution']['y'] = int(
-            collection['properties']['bdc:bands'][first_band]['resolution_y']) * -1
+        _pelement = element
+        for tree_node in tree_path:
+            # walking through tree nodes
+            if tree_node not in _pelement:
+                _pelement[tree_node] = OrderedDict()
+                if tree_node == tree_path[-1]:
+                    _pelement[tree_node] = value
+            _pelement = _pelement[tree_node]
 
-        odc_config['metadata'] = OrderedDict()
-        odc_config['metadata']['platform'] = {'code': kwargs['platform_code']}
-        odc_config['metadata']['instrument'] = {'name': kwargs['instrument_type']}
-        odc_config['metadata']['product_type'] = self.generate_product_type(collection)
-        odc_config['metadata']['format'] = {'name': kwargs['format_name']}
-        odc_config['measurements'] = [measurements(k, v)
-                                      for k, v in collection['properties']['bdc:bands'].items() if
-                                      k not in kwargs['ignore']]
-
-        if kwargs['verbose']:
-            logger.info("Finished!")
-        return odc_config
-
-    def map_dataset(self, collection, dataset_items, **kwargs) -> OrderedDict:
-        from osgeo import gdal
-        from datetime import datetime
-        import stac2odc.environment as environment
-
-        if kwargs['verbose']:
-            logger.info("item2dataset is running!")
-
-        crs_proj4 = collection['properties']['bdc:crs']
-        if kwargs['is_pre_collection']:
-            crs_proj4 = utils.fix_precollection_crs(crs_proj4)
-
-        odc_items = []
-        for f in dataset_items:
-            try:
-                _startdate, _enddate = utils.stacdate_to_odcdate(f['id'])
-            except:
-                _tmp = f['properties']['datetime']
-                _startdate, _enddate = _tmp, _tmp
-
-            _featureid = utils.generate_id(f)
-            if kwargs['verbose']:
-                logger.info(f"New item found: {_featureid}")
-
-            feature = OrderedDict()
-            feature['id'] = _featureid
-            feature['creation_dt'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%fZ")
-            feature['product_type'] = self.generate_product_type(collection)
-            feature['platform'] = {'code': kwargs['plataform_code']}
-            feature['instrument'] = {'name': kwargs['instrument_type']}
-            feature['format'] = {'name': kwargs['format_name']}
-            feature['lineage'] = {'source_datasets': {}}
-
-            feature['extent'] = OrderedDict()
-            feature['extent']['coord'] = OrderedDict()
-            feature['extent']['coord'] = utils.geometry_coordinates(f)
-            feature['extent']['from_dt'] = _startdate
-            feature['extent']['center_dt'] = _startdate  # ToDo: Verify this
-            feature['extent']['to_dt'] = _enddate
-
-            # verify if necessary download data
-            if kwargs['download']:
-                logger.info(f"Downloading item: {_featureid}")
-                environment.download_stac_tree(f, **kwargs)
-                kwargs['basepath'] = kwargs['download_out']
-
-            # Extract image bbox
-            first_band = next(iter(collection['properties']['bdc:bands']))
-            first_band_path = utils.href_to_path(
-                f['assets'][first_band]['href'], kwargs['basepath'])
-
-            # build grid_spatial
-            lrx, lry, ulx, uly = utils.raster_bounds(first_band_path)
-            feature['grid_spatial'] = OrderedDict()
-            feature['grid_spatial']['projection'] = OrderedDict()
-            feature['grid_spatial']['projection']['geo_ref_points'] = OrderedDict()
-            feature['grid_spatial']['projection']['geo_ref_points']['ul'] = {
-                'x': ulx, 'y': uly}
-            feature['grid_spatial']['projection']['geo_ref_points']['ur'] = {
-                'x': lrx, 'y': uly}
-            feature['grid_spatial']['projection']['geo_ref_points']['lr'] = {
-                'x': lrx, 'y': lry}
-            feature['grid_spatial']['projection']['geo_ref_points']['ll'] = {
-                'x': ulx, 'y': lry}
-
-            feature['grid_spatial']['projection']['spatial_reference'] = utils.to_wkt(crs_proj4)
-            feature['image'] = OrderedDict()
-            feature['image']['bands'] = OrderedDict()
-            band_counter = 1
-            for band in collection['properties']['bdc:bands'].keys():
-                if band not in kwargs['ignore']:
-                    if band in f['assets']:
-                        feature['image']['bands'][band] = OrderedDict()
-                        feature['image']['bands'][band]['path'] = utils.href_to_path(
-                            f['assets'][band]['href'], kwargs['basepath'])
-                        feature['image']['bands'][band]['layer'] = 1
-                        band_counter += 1
-                    else:
-                        logger.info("Band '{}' was not found in asset '{}'".format(
-                            band, f['id']))
-            odc_items.append(feature)
-        return odc_items
+    @classmethod
+    def get_registered_engines(cls):
+        return cls.DEFAULT_ENGINE_DEFINITION.copy()
 
 
-class Stac2ODCMapper09(Stac2ODCMapper):
+if __name__ == '__main__':
+    import os
+    import yaml
+    import stac
 
-    def generate_product_type(self, collectionobj):
-        if collectionobj['bdc:temporal_composition']:
-            return "{}_{}_{}".format(
-                collectionobj['bdc:temporal_composition']['schema'],
-                collectionobj['bdc:temporal_composition']['step'],
-                collectionobj['bdc:temporal_composition']['unit'])
-        return None
+    stac_service = stac.STAC('http://brazildatacube.dpi.inpe.br/stac/', False)
+    stac_collection = stac_service.collection('CB4_64_16D_STK-1')
 
-    def map_collection(self, collection, **kwargs) -> OrderedDict:
-        def measurements(data):
-            m = OrderedDict()
-            m['name'] = data['name'].replace('-', '_')
-            m['aliases'] = [data['name'], ]
-            m['dtype'] = data['data_type'].lower()
-            m['nodata'] = data['nodata']
-            m['units'] = kwargs['units']
-            return m
+    # custom_fields_definition = json.load(open(os.path.join(os.getcwd(), 'json-schemas/custom-field-definition.json')))
 
-        if kwargs['verbose']:
-            logger.info("collection2product is running!")
+    engine = StacMapperEngine(os.path.join(os.getcwd(), 'engine-definitions/stac_mapper_v09.json'))
+    odc_product = engine.map_collection_to_product(stac_collection)
 
-        crs_proj4 = collection['cube:dimensions']['x']['reference_system']
-        if kwargs['is_pre_collection']: # fix gdal3 proj4 pattern error
-            crs_proj4 = utils.fix_precollection_crs(crs_proj4)
-
-        odc_config = OrderedDict()
-        odc_config['name'] = collection['id'].replace('-', '_')
-        odc_config['description'] = collection['description']
-        odc_config['metadata_type'] = kwargs['metadata_type']
-
-        odc_config['storage'] = OrderedDict()
-        odc_config['storage']['crs'] = crs_proj4
-        odc_config['storage']['resolution'] = OrderedDict()
-        odc_config['storage']['resolution']['x'] = int(collection['properties']['eo:gsd'])
-        odc_config['storage']['resolution']['y'] = int(collection['properties']['eo:gsd']) * -1
-
-        # Generate platform infos
-        odc_config['metadata'] = OrderedDict()
-        odc_config['metadata']['platform'] = {'code': kwargs['platform_code']}
-        odc_config['metadata']['instrument'] = {'name': kwargs['instrument_type']}
-        odc_config['metadata']['product_type'] = self.generate_product_type(collection)
-        odc_config['metadata']['format'] = {'name': kwargs['format_name']}
-        odc_config['measurements'] = [measurements(v)
-                                      for v in collection['properties']['eo:bands'] if
-                                      v['common_name'] not in kwargs['ignore']]
-
-        if kwargs['verbose']:
-            logger.info("Finished!")
-        return odc_config
-
-    def map_dataset(self, collection, dataset_items, **kwargs) -> OrderedDict:
-        from datetime import datetime
-        import stac2odc.environment as environment
-
-        if kwargs['verbose']:
-            logger.info("item2dataset is running!")
-
-        crs_proj4 = collection['cube:dimensions']['x']['reference_system']
-        if kwargs['is_pre_collection']:
-            crs_proj4 = utils.fix_precollection_crs(crs_proj4)
-
-        odc_items = []
-        for f in dataset_items:
-            try:
-                _startdate, _enddate = utils.stacdate_to_odcdate(f['id'])
-            except:
-                _tmp = f['properties']['datetime']
-                _startdate, _enddate = _tmp, _tmp
-
-            _featureid = utils.generate_id(f)
-            if kwargs['verbose']:
-                logger.info(f"New item found: {_featureid}")
-
-            feature = OrderedDict()
-            feature['id'] = _featureid
-            feature['creation_dt'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%fZ")
-            feature['product_type'] = self.generate_product_type(collection)
-            feature['platform'] = {'code': kwargs['plataform_code']}
-            feature['instrument'] = {'name': kwargs['instrument_type']}
-            feature['format'] = {'name': kwargs['format_name']}
-            feature['lineage'] = {'source_datasets': {}}
-
-            feature['extent'] = OrderedDict()
-            feature['extent']['coord'] = OrderedDict()
-            feature['extent']['coord'] = utils.geometry_coordinates(f)
-            feature['extent']['from_dt'] = _startdate
-            feature['extent']['center_dt'] = _startdate # ToDo: Verify this
-            feature['extent']['to_dt'] = _enddate
-
-            # verify if necessary download data
-            if kwargs['download']:
-                logger.info(f"Downloading item: {_featureid}")
-                environment.download_stac_tree(f, **kwargs)
-                kwargs['basepath'] = kwargs['download_out']
-
-            # Extract image bbox
-            first_band = next(iter(collection['properties']['eo:bands']))
-            first_band_path = utils.href_to_path(
-                f['assets'][first_band['name']]['href'], kwargs['basepath'])
-
-            # build grid_spatial
-            lrx, lry, ulx, uly = utils.raster_bounds(first_band_path)
-            feature['grid_spatial'] = OrderedDict()
-            feature['grid_spatial']['projection'] = OrderedDict()
-            feature['grid_spatial']['projection']['geo_ref_points'] = OrderedDict()
-            feature['grid_spatial']['projection']['geo_ref_points']['ul'] = {
-                'x': ulx, 'y': uly}
-            feature['grid_spatial']['projection']['geo_ref_points']['ur'] = {
-                'x': lrx, 'y': uly}
-            feature['grid_spatial']['projection']['geo_ref_points']['lr'] = {
-                'x': lrx, 'y': lry}
-            feature['grid_spatial']['projection']['geo_ref_points']['ll'] = {
-                'x': ulx, 'y': lry}
-
-            feature['grid_spatial']['projection']['spatial_reference'] = utils.to_wkt(crs_proj4)
-            feature['image'] = OrderedDict()
-            feature['image']['bands'] = OrderedDict()
-            band_counter = 1
-            for bandindex in collection['properties']['eo:bands']:
-                band = bandindex['name']
-                if band not in kwargs['ignore']:
-                    if band in f['assets']:
-                        feature['image']['bands'][band] = OrderedDict()
-                        feature['image']['bands'][band]['path'] = utils.href_to_path(
-                            f['assets'][band]['href'], kwargs['basepath'])
-                        feature['image']['bands'][band]['layer'] = 1
-                        band_counter += 1
-                    else:
-                        logger.info("Band '{}' was not found in asset '{}'".format(
-                            band, f['id']))
-            odc_items.append(feature)
-        return odc_items
-"""
+    with open('stac_collection.yaml', 'w') as f:
+        yaml.dump(odc_product, f)
