d495edf154a3ea2b4391cb084a31ca9e225a59f8
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-10-16 17:27:40 -0300

    generalize mapper to accept items and collections

--- stac2odc/mapper.py
+++ stac2odc/mapper.py
@@ -14,6 +14,58 @@ from stac2odc.operation import apply_custom_map_function
 from stac2odc.toolbox import load_custom_configuration_file
 
 
+def _apply_custom_mapping(stac_values: list, custom_mapping: dict) -> object:
+    """Function to map custom definitions in mapping function. With this functions is possible use STAC
+        definition in arbitrary fields
+    Args:
+        stac_values (list): List of STAC Items
+        custom_mapping (dict): Dict with custom mapping, where key is used in ODC's definition and values is
+        recovered from STAC elements
+    Returns:
+    """
+
+    def apply_custom_mapping_in_a_list(stac_values: list, custom_mapping: dict):
+        """Apply custom mapping function in a list of elements
+        Args:
+            stac_values (list): List of STAC Items
+            custom_mapping (dict): Dict with custom mapping, where key is used in ODC's definition and values is
+            recovered from STAC elements
+
+        Returns:
+            list: List with STAC Items mapped
+        """
+        stac_values_with_custom_fields = []
+        for _stac_value in stac_values:
+            _stac_value_to_map = OrderedDict()
+
+            for custom_key_mapping in custom_mapping:
+                _stac_value_to_map[custom_key_mapping] = _stac_value.get(
+                    custom_mapping.get(custom_key_mapping)
+                )
+            stac_values_with_custom_fields.append(_stac_value_to_map)
+        return stac_values_with_custom_fields
+
+    def apply_custom_mapping_in_a_dict(stac_values: dict, custom_mapping: dict):
+        def replace_by_key(text, to, src="$key") -> str:
+            return text.replace(src, to)
+
+        stac_values_with_custom_fields = OrderedDict()
+        for stac_value_key in stac_values:
+            _stac_value = stac_values.get(stac_value_key)
+
+            for custom_key_mapping in custom_mapping:
+                tree_path = ".".join(replace_by_key(custom_key_mapping, stac_value_key).split(".")[1:])
+                tree.add_value_by_tree_path(stac_values_with_custom_fields, tree_path, _stac_value.get(
+                    custom_mapping.get(custom_key_mapping)
+                ))
+        return stac_values_with_custom_fields
+
+    _fnc = apply_custom_mapping_in_a_dict
+    if isinstance(stac_values, list):
+        _fnc = apply_custom_mapping_in_a_list
+    return _fnc(stac_values, custom_mapping)
+
+
 class StacMapperEngine:
     def __init__(self, engine_definition_file=None):
         """StacMapperEngine is a core of stac2odc tool. An engine is able to map the STAC definition to ODC definition
@@ -22,56 +74,38 @@ class StacMapperEngine:
         """
         self._engine_definition = load_custom_configuration_file(engine_definition_file)
 
-    def map_collection_to_product(self, stac_collection: dict):
-        """Method to map STAC Collections to Products using engine's selected rules
+    def _map_stac_element_to_odc_element(self, stac_element: dict, odc_element_type: str):
+        """General function to map STAC Element to ODC Element based on StacMapper's rules
         Args:
-            stac_collection (dict): STAC Collection properties
+            stac_element (dict): STAC Collection properties
+            odc_element_type (str): Type of ODC element (dataset or collection)
         Returns:
             OrderedDict: ODC Product created using STAC Collection definitions
         """
 
-        def apply_custom_mapping_in_stac_values(stac_values: list, custom_mapping: dict):
-            """Function to map custom definitions in mapping function. With this functions is possible use STAC
-            definition in arbitrary fields
-            Args:
-                stac_values (list): List of STAC Items
-                custom_mapping (dict): Dict with custom mapping, where key is used in ODC's definition and values is
-                recovered from STAC elements
-
-            Returns:
-                list: List with STAC Items mapped
-            """
-            stac_values_with_custom_fields = []
-            for _stac_value in stac_values:
-                _stac_value_to_map = OrderedDict()
-
-                for custom_key_mapping in custom_mapping:
-                    _stac_value_to_map[custom_key_mapping] = _stac_value.get(
-                        custom_mapping.get(custom_key_mapping)
-                    )
-                stac_values_with_custom_fields.append(_stac_value_to_map)
-            return stac_values_with_custom_fields
+        if not self._engine_definition.get(odc_element_type, None):
+            raise ODCInvalidType(f"ODC Type {odc_element_type} is not avaliable")
 
         odc_product_definition = OrderedDict()
-        collection_mapper = self._engine_definition.get('collection')
-        product_definition = collection_mapper.get('fromSTAC')
+        element_mapper = self._engine_definition.get(odc_element_type)
+        product_definition = element_mapper.get('fromSTAC')
 
         for product_property in product_definition:
             property_definition = product_definition.get(product_property)
 
             if 'customMapping' in property_definition:
-                stac_value = tree.get_value_by_tree_path(stac_collection, property_definition.get('from'))
-                stac_value = apply_custom_mapping_in_stac_values(stac_value, property_definition.get('customMapping'))
+                stac_value = tree.get_value_by_tree_path(stac_element, property_definition.get('from'))
+                stac_value = _apply_custom_mapping(stac_value, property_definition.get('customMapping'))
             elif 'customMapFunction' in property_definition:
                 property_is_from = property_definition.get('from')
 
-                stac_value = tree.get_value_by_tree_path(stac_collection, property_is_from)
+                stac_value = tree.get_value_by_tree_path(stac_element, property_is_from)
                 stac_value = apply_custom_map_function(property_is_from, stac_value,
                                                        property_definition.get('customMapFunction'))
             else:  # normal code
-                stac_value = tree.get_value_by_tree_path(stac_collection, product_definition.get(product_property))
+                stac_value = tree.get_value_by_tree_path(stac_element, product_definition.get(product_property))
             tree.add_value_by_tree_path(odc_product_definition, product_property, stac_value)
-        return self._add_custom_fields_to_odc_element(odc_product_definition, "collection")
+        return self._add_custom_fields_to_odc_element(odc_product_definition, odc_element_type)
 
     def _add_custom_fields_to_odc_element(self, odc_element: OrderedDict, odc_element_type: str) -> OrderedDict:
         """Add custom fields into ODC Elements (Products or Datasets) in arbitrary tree paths
@@ -82,9 +116,6 @@ class StacMapperEngine:
         Returns:
             OrderedDict: ODC Element with custom fields inserted
         """
-        if not self._engine_definition.get(odc_element_type, None):
-            raise ODCInvalidType(f"ODC Type {odc_element_type} is not avaliable in custom fields definition!")
-
         mapper = self._engine_definition.get(odc_element_type)
 
         from_file_definitions = mapper.get('fromFile', None)
@@ -116,11 +147,22 @@ class StacMapperEngine:
 
         return odc_element
 
-    def map_item_to_dataset(self, stac_collection: dict):
+    def map_collection_to_product(self, stac_collection: dict):
         """
         Args:
-            stac_collection:
+            stac_collection (dict): STAC Collection properties
 
         Returns:
+            OrderedDict: ODC Product created using STAC Collection definitions
         """
-        pass
+
+        return self._map_stac_element_to_odc_element(stac_collection, "product")
+
+    def map_item_to_dataset(self, stac_item: dict):
+        """
+        Args:
+            stac_item (dict): STAC Item properties
+        Returns:
+        """
+
+        return self._map_stac_element_to_odc_element(stac_item, "dataset")
